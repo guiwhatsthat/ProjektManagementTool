cb7f49f3571d7eeb0e08ca7302b5ca784a970f50
stevengasser <steven.gasser@bluewin.ch>
2021-02-21 23:00:36 +0100

    [ADD] Biniding für AktiviteatBearbeitenView

---
+++ ProjektManagementTool/ProjektManagementTool/ViewModels/AktiviteatBearbeitenViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjektManagementTool.ViewModels
+{
+    class AktiviteatBearbeitenViewModel : BaseViewModel
+    {
+    }
+}
